1443a35b7a3e56ed882178da21255e3057d27c60
Michele <50552288+PokeMichele@users.noreply.github.com>
2021-11-18 20:24:14 +0100

    LogDel8 1.0

---
+++ LogDel8/LogDel8/src/logdel8/PluginListener.java
@@ -0,0 +1,11 @@
+package logdel8;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+@SuppressWarnings("unused")
+
+public class PluginListener implements Listener {
+
+}
